c186eb8c3e33f91275dce5b8d340663049e02b69
Manohar <rajmanohar19@gmail.com>
2023-05-09 17:24:02 +0000

    Tue May  9 17:24:02 UTC 2023

--- website-script/webscript.sh
+++ website-script/webscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install apache2 wget unzip -y
-systemctl start apache2
-systemctl enable apache2
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-unzip 2117_infinite_loop.zip
-cp 2117_infinite_loop/. /var/www/html/.
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+sudo unzip 2117_infinite_loop.zip
+sudo cp 2117_infinite_loop/. /var/www/html/.
 
