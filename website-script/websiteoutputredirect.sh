0681c74424d0876ea1f54a0b53fa5f197a1dd19a
Manohar <rajmanohar19@gmail.com>
2023-05-10 18:02:01 +0000

    Wed May 10 18:02:01 UTC 2023

--- website-script/websiteoutputredirect.sh
+++ website-script/websiteoutputredirect.sh
@@ -9,22 +9,27 @@ fi
 if [ $number -ge 0 ] && [ $number -le 10 ]; then
 echo "apache2 start"
 sudo systemctl start apache2 &> /dev/null
+sleep 10s
 echo "apache2 enable"
 sudo systemctl enable apache2 &> /dev/null
 mkdir -p new-web
 cd new-web
+sleep 10s
 echo "downloading teamplate...."
 sudo wget $url &> /dev/null
 name=$(basename "$url" .zip)
 echo "tooplate name is $name"
+sleep 10s
 echo "unzip of tooplate template"
 sudo unzip $name.zip &> /dev/null
 sudo rm -rf /var/www/html/*
 sudo cp -r $name/* /var/www/html/.
 cd ..
 sudo rm -rf new-web
+sleep 10s
 echo "restart apache2"
 sudo systemctl restart apache2
+
 else
 echo "!*!*!*!*!**!*!*!*!*!*!*!*!*!*!*!*!*!*!"
 fi
