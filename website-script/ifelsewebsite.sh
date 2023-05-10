3ad80f1a78d6e1a464746b773f06d24ff61f0bc6
Manohar <rajmanohar19@gmail.com>
2023-05-10 17:12:01 +0000

    Wed May 10 17:12:01 UTC 2023

--- website-script/ifelsewebsite.sh
+++ website-script/ifelsewebsite.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+read -p "Enter a number between 0 to 10: " number 
 echo "enter any tooplate url"
 read url
-if [ $url -eq 0 ]; then
+if [ $number -ge 0 ] && [ $number -le 5 ]; then
 
 sudo apt install apache2 wget unzip -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
 mkdir -p new-web
 cd new-web
-sudo wget https://www.tooplate.com/zip-templates/2118_chilling_cafe.zip
-sudo unzip 2118_chilling_cafe.zip
+sudo wget $url
+name=$(basename "$url" .zip)
+echo "$name"
+sudo unzip $name.zip
 sudo rm -rf /var/www/html/*
-sudo cp -r 2118_chilling_cafe/* /var/www/html/.
+sudo cp -r $name/* /var/www/html/.
 cd ..
 sudo rm -rf new-web
 sudo systemctl restart apache2
@@ -23,10 +26,12 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 mkdir -p new-web
 cd new-web
-sudo wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-sudo unzip 2117_infinite_loop.zip
+sudo wget $url
+name=$(basename "$url" .zip)
+echo "$name"
+sudo unzip $name.zip
 sudo rm -rf /var/www/html/*
-sudo cp -r 2117_infinite_loop/* /var/www/html/.
+sudo cp -r $name/* /var/www/html/.
 cd ..
 sudo rm -rf new-web
 sudo systemctl restart apache2
