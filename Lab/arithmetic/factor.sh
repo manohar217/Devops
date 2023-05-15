a87ce2988f792f262dbd138dd5aba17d5c2cd001
Manohar <rajmanohar19@gmail.com>
2023-05-15 12:48:01 +0000

    Mon May 15 12:48:01 UTC 2023

--- Lab/arithmetic/factor.sh
+++ Lab/arithmetic/factor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "give a number"
 read number
-if ($number%2 ==0)
+if ($number%2 ==0);
 echo "one factor"
+
 elif ($number%3==0)
 echo "one factor...actually two!"
+
 else
+	
 echo "2 and 3 are not factors of $number"
+
